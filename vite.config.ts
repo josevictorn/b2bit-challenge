0827d8deafa8e298027153d42ab0752abc73a1b6
victor <josevictornascimento2016@gmail.com>
2024-05-17 15:23:54 -0300

    :test_tube: test: creates unit and integration test environment setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { UserConfig } from 'vite'
+import type { InlineConfig } from 'vitest'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
