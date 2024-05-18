74e6b77426f6c23f117bfdb3febd3b49ce39f05a
victor <josevictornascimento2016@gmail.com>
2024-05-17 22:01:23 -0300

    :test_tube: test: create tests for the component button

---
+++ src/components/button.spec.tsx
@@ -0,0 +1,23 @@
+import { render } from '@testing-library/react'
+import { Button } from './button'
+
+describe('Button component', async () => {
+  it('should render button', () => {
+    const { getByText, getByRole } = render(<Button title='Button' />)
+
+    const buttonElement = getByRole('button')
+    const buttonTextElement = getByText('Button')
+
+
+    expect(buttonElement).toBeInTheDocument()
+    expect(buttonTextElement).toBeInTheDocument()
+  })
+
+  it('should apply the correct width class when size is passed', () => {
+    const { getByRole } = render(<Button size={56} title='Button' />)
+
+    const buttonElement = getByRole('button')
+
+    expect(buttonElement).toHaveClass('w-56')
+  })
+})
